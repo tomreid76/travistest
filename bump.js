d9fde12b93fce8d674f881c22ed24cc710795788
Tom Reid <treid@forever.com>
2016-11-07 09:08:10 -0500

    Finalize bump script.

--- bump.js
+++ bump.js
@@ -3,46 +3,44 @@ const cp = require('child_process');
 const CURRENT_VERSION = `v${require('./package.json').version}`;
 const PREVIOUS_VERSION = cp.execSync('git describe --abbrev=0', { encoding: 'utf8' }).trim();
 const isPullRequest = process.env.TRAVIS_PULL_REQUEST && JSON.parse(process.env.TRAVIS_PULL_REQUEST);
-const eventType = process.env.TRAVIS_EVENT_TYPE;
+const isTravis = process.env.TRAVIS && JSON.parse(process.env.TRAVIS);
 
-//cp.execSync(`git config --global user.email "foreverbuild@travis-ci.com"`);
-//cp.execSync(`git config --global user.name "TravisCI"`);
+/*make sure this is being run on travis. If so, set git config accordingly.*/
+if (isTravis) {
+  console.log('Setting git username and email to environment...');
+  cp.execSync(`git config --global user.email "foreverbuild@travis-ci.com"`);
+  cp.execSync(`git config --global user.name "TravisCI"`);
+} else {
+  console.log('ERROR: This script is intended to be run on TravisCI only.');
+  process.exit(1);
+}
 
-/*
-console.log('event type: ', eventType);
-console.log('travis commit: ', process.env.TRAVIS_COMMIT);
-console.log('travis commit range: ', process.env.TRAVIS_COMMIT_RANGE);
-console.log('travis tag: ', process.env.TRAVIS_TAG);
-*/
-
+/*if not a PR. attempt bump and tag*/
 if (!isPullRequest) {
+
+  /*package file version is equal to the last git tag version, attempt autobump of patch*/
   if (PREVIOUS_VERSION === CURRENT_VERSION) {
-    //autobump
+    console.log('No version set, attempting autobump of patch...');
     cp.exec('npm version patch -m \"Travis Autobump [skip ci]\"', { encoding: 'utf8' }, (error, stdout, stderr) => {
       if (error) {
-        console.log('Something went wrong, aborting', stderr);
+        console.log('Something went wrong while autobumping, aborting', stderr);
         process.exit(1);
       }
       const NEW_VERSION = stdout.trim();
       console.log(`Auto bumping patch from ${CURRENT_VERSION} to ${NEW_VERSION}`);
       cp.execSync(`git push origin master --tags`);
+      console.log('Bump successful. New version tag pushed to master.');
     });
 
-    //process.exit(1);
+  /*package version has been update, attempt tag only.*/
   } else {
     console.log(`Manual bump here from ${PREVIOUS_VERSION} to ${CURRENT_VERSION}`);
     cp.execSync(`git tag -a ${CURRENT_VERSION} -m "Travis Autobump [skip ci]"`);
     cp.execSync(`git push origin master --tags`);
+    console.log('Bump successful. New version tag pushed to master.');
   }
 
+/*this is a PR, do nothing*/
 } else {
   console.log('Pull request build run. Skipping tagging operation...');
 }
-
-
-
-/**/
-
-//console.log('Is a pr:' + isPullRequest);
-//git tag -a %build.number% -m "Version %build.number%"
-//git push origin %sapphire-client-release-branch% --tags
